fe76b7b5ed31a13572e11f24745043986351c587
Derrick <derrickryan10@gmail.com>
2022-06-26 10:00:47 -0400

    Add analyzeArray

---
+++ analyzeArray.js
@@ -0,0 +1,11 @@
+function analyzeArray(arr) {
+    object = {
+        average: arr.reduce((a, b) => a + b) / arr.length,
+        min: Math.min(...arr),
+        max: Math.max(...arr),
+        length: arr.length
+    };
+    return object;
+}
+
+module.exports = analyzeArray;
